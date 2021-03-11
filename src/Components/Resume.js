ef04e94327699f9e19dc86d14433a12e9a172b9e
adt315 <angela_dyan@hotmail.com>
2021-03-11 10:17:43 -0500

    edit skills

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -16,6 +16,10 @@ class Resume extends Component {
             <p>{work.description}</p>
         </div>
       })
+      var skills = this.props.data.skills.map(function(skills){
+        var className = 'bar-expand '+skills.name.toLowerCase();
+        return <li key={skills.name}><span style={{width:skills.className}}className={className}></span><em>{skills.name}</em></li>
+      })
     }
 
     return (
@@ -52,7 +56,7 @@ class Resume extends Component {
       <div className="row skill">
 
          <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
+            <h1><span>Top Skills</span></h1>
          </div>
 
          <div className="nine columns main-col">
@@ -62,7 +66,7 @@ class Resume extends Component {
 
 				  <div className="bars">
 				   <ul className="skills">
-					  {/* {skills} */}
+					   {skills}
 					</ul>
 				</div>
 			</div>
